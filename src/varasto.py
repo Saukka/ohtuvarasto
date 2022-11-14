5420d109544fb460e171b449ea75a8e24f660e68
Saukka <sauli.sorvari@kolumbus.fi>
2022-11-14 20:13:14 +0200

    t3 staattinen analyysi

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,17 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
+        self.tilavuus = 0.0
+        self.saldo = 0.0
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        if alku_saldo > 0:
+            self.aseta_saldo(tilavuus, alku_saldo)
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
+    def aseta_saldo(self, tilavuus, alku_saldo):
+        self.saldo = tilavuus
+        if alku_saldo <= tilavuus:
             self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
-
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea ilman ylimääräisiä kenttiä.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
